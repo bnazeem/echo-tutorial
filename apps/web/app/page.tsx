d01ffc5f1867f2edecf02df479d500689b66602f
Nazeem Bahadoor <bnazeem@gmail.com>
2025-08-09 20:15:42 -0400

    03: clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,37 @@
 "use client";
-import { useMutation, useQuery } from "convex/react";
+import {
+  useMutation,
+  useQuery,
+  Authenticated,
+  Unauthenticated,
+} from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
+import {} from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p>apps/web</p>
-        <Button onClick={() => addUser()}>Add</Button>
-        <div className="max-w-sm w-full mx-auto ">
-          {JSON.stringify(users, null, 2)}
+    <>
+      <Authenticated>
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <UserButton />
+            <p>apps/web</p>
+            <Button onClick={() => addUser()}>Add</Button>
+            <div className="max-w-sm w-full mx-auto ">
+              {JSON.stringify(users, null, 2)}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>Must be signed in!</p>
+        <SignInButton>Sign in</SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
