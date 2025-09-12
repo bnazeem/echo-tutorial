09e11507767071b1b0dafd47b7cd0a253d8e54aa
Nazeem Bahadoor <bnazeem@gmail.com>
2025-09-11 21:50:07 -0400

    16 widget inbox

--- apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -14,6 +14,7 @@ import {
 import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { useState } from "react";
+import { WidgetFooter } from "../components/widget-footer";
 
 export const WidgetSelectionScreen = () => {
   const setScreen = useSetAtom(screenAtom);
@@ -80,6 +81,7 @@ export const WidgetSelectionScreen = () => {
           </div>
         </Button>
       </div>
+      <WidgetFooter />
     </>
   );
 };
