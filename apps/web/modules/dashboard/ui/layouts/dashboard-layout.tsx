c3114b79f603d77a693e4e054d42b0f072eca48e
Nazeem Bahadoor <bnazeem@gmail.com>
2025-09-21 18:09:10 -0400

    18: dashboard chat

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -2,6 +2,7 @@ import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
 import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import { SidebarProvider } from "@workspace/ui/components/sidebar";
+import { Provider } from "jotai";
 import { cookies } from "next/headers";
 
 export const Dashboardlayout = async ({
@@ -10,15 +11,19 @@ export const Dashboardlayout = async ({
   children: React.ReactNode;
 }) => {
   const cookieStore = await cookies();
+  // Using SIDEBAR_COOKIE_NAME from sidebar component does not work due to monorepo and SSR
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+  // const defaultOpen = cookieStore.get(SIDEBAR_COOKIE_NAME)?.value === "true";
 
   return (
     <AuthGuard>
       <OrganizationGuard>
-        <SidebarProvider defaultOpen={defaultOpen}>
-          <DashboardSidebar />
-          <main className="flex flex-1 flex-col">{children}</main>
-        </SidebarProvider>
+        <Provider>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <DashboardSidebar />
+            <main className="flex flex-1 flex-col">{children}</main>
+          </SidebarProvider>
+        </Provider>
       </OrganizationGuard>
     </AuthGuard>
   );
