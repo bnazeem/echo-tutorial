5a00e99462d28eacc69b9deb811bfbb8eab32a89
Nazeem Bahadoor <bnazeem@gmail.com>
2025-09-28 19:31:53 -0400

    19: ai tool calling

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -2,7 +2,7 @@ import { openai } from "@ai-sdk/openai";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api";
 
-export const supportAgent = new Agent(components.agent, {
+export const supportAgent: any = new Agent(components.agent, {
   chat: openai.chat("gpt-4o-mini"),
-  instructions: "You are a customer support agent",
+  instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. Use "escalateConversation" tool when user expresses frustration, or requests a human explicitly`,
 });
